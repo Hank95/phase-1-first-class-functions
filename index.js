0c4e202c310e47d224d180b880402e676bb9b0c8
github username <github email address>
2021-06-08 19:48:24 -0400

    commit tree bdfec71df9dda4ca6fe0c597a77d335a7b653d7b

---
+++ index.js
@@ -0,0 +1,56 @@
+function runFiveMiles() {
+  console.log("Go for a five-mile run");
+}
+
+function liftWeights() {
+  console.log("Pump iron");
+}
+
+function swimFortyLaps() {
+  console.log("Swim 40 laps");
+}
+function exerciseRoutine(postRunActivity) {
+  runFiveMiles();
+  postRunActivity();
+}
+exerciseRoutine(() => console.log("Stretch! Work that core!"));
+
+function morningRoutine(exercise) {
+  let breakfast;
+
+  if (exercise === liftWeights) {
+    breakfast = "protein bar";
+  } else if (exercise === swimFortyLaps) {
+    breakfast = "kale smoothie";
+  } else {
+    breakfast = "granola";
+  }
+
+  exerciseRoutine(exercise);
+
+  // we could give this function a name if we wanted to, but since
+  // it's only available _inside_ morningRoutine(), we don't need to
+  return function () {
+    console.log(`Nom nom nom, this ${breakfast} is delicious!`);
+  };
+}
+const afterExercise = morningRoutine(liftWeights);
+
+afterExercise();
+
+function receivesAFunction(callback) {
+  return callback();
+}
+
+const returnsANamedFunction = function () {
+  function task() {
+    return "end task";
+  }
+  return task;
+};
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("function");
+  };
+}
